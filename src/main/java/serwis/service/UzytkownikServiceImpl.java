d1cac5a7584bcb173516c61f7312084e08d44fd0
Julia Komorowska <komolcia@gmail.com>
2022-05-10 23:23:29 +0200

    Added Data to Repository

--- src/main/java/serwis/service/UzytkownikServiceImpl.java
+++ src/main/java/serwis/service/UzytkownikServiceImpl.java
@@ -1,11 +1,30 @@
 package serwis.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import serwis.model.Konferencja;
 import serwis.model.Prelekcja;
+import serwis.model.Sciezka;
 import serwis.model.Uzytkownik;
+import serwis.repository.KonferencjaRepository;
+import serwis.repository.PrelekcjaRepository;
+import serwis.repository.SciezkaRepository;
 import serwis.repository.UzytkownikRepository;
+
+import java.time.LocalDateTime;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 public class UzytkownikServiceImpl implements  UzytkownikService {
+    @Autowired
+    SciezkaRepository sciezkaRepository;
+    @Autowired
+    KonferencjaRepository konferencjaRepository;
+    @Autowired
+    PrelekcjaRepository prelekcjaRepository;
    public UzytkownikRepository uzytkownikRepository;
    public UzytkownikServiceImpl(UzytkownikRepository uzytkownikRepository){this.uzytkownikRepository=uzytkownikRepository}
     @Override
@@ -33,7 +52,38 @@ public class UzytkownikServiceImpl implements  UzytkownikService {
         uzytkownikRepository.save(uzytkownik);
     }
     public void makeKonferencja(){
-       Prelekcja prelekcja1 = new Prelekcja()
+      LocalDateTime data1= LocalDateTime.parse("2021-06-01T00:00:00");
+        LocalDateTime data2= LocalDateTime.parse("2021-06-01T10:00:00");
+        LocalDateTime data3= LocalDateTime.parse("2021-06-01T11:45:00");
+        LocalDateTime data4= LocalDateTime.parse("2021-06-02T12:00:00");
+        LocalDateTime data5= LocalDateTime.parse("2021-06-01T13:45:00");
+        LocalDateTime data6= LocalDateTime.parse("2021-06-02T14:00:00");
+        LocalDateTime data7= LocalDateTime.parse("2021-06-01T15:45:00");
+        LocalDateTime data8= LocalDateTime.parse("2021-06-02T00:00:00");
+        Set<Uzytkownik> uzytkownik= new HashSet<>();
+        Prelekcja prelekcja1 = new Prelekcja(data2,data3);
+        Prelekcja prelekcja2 = new Prelekcja(data4,data5);
+        Prelekcja prelekcja3 = new Prelekcja(data6,data7);
+        Set<Prelekcja> prelekcjaSet= new HashSet<>();
+        prelekcjaSet.add(prelekcja1);
+        prelekcjaSet.add(prelekcja2);
+        prelekcjaSet.add(prelekcja3);
+        Sciezka sciezka1=new Sciezka("Java",prelekcjaSet);
+        Sciezka sciezka2=new Sciezka("Dot Net",prelekcjaSet);
+        Sciezka sciezka3=new Sciezka("C++",prelekcjaSet);
+        Set<Sciezka> sciezkaSet=new HashSet<>();
+        sciezkaSet.add(sciezka1);
+        sciezkaSet.add(sciezka2);
+        sciezkaSet.add(sciezka3);
+        Konferencja konferencja=new Konferencja(data1,data8,sciezkaSet);
+        prelekcjaRepository.save(prelekcja1);
+        prelekcjaRepository.save(prelekcja2);
+        prelekcjaRepository.save(prelekcja3);
+        sciezkaRepository.save(sciezka1);
+        sciezkaRepository.save(sciezka2);
+        sciezkaRepository.save(sciezka3);
+        konferencjaRepository.save(konferencja);
+
     }
 
 }
